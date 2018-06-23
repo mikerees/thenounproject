ea04e026661849e8fab132e2e14b798f5e4054b3
Mike Rees <mike@tatelu.com>
2018-06-23 10:15:21 +0100

    Remove URI from toArray() method in AbstractModel

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -7,12 +7,20 @@ class AbstractModel
 {
 
     /**
+     * @var string The endpoint in the API
+     */
+    public $uri;
+
+    /**
      * Get the properties of the model as an associative array, excluding URI and Method
      * @return array
      */
     public function toArray()
     {
-        return get_object_vars($this);
+        $vars = get_object_vars($this);
+        unset($vars["uri"]);
+
+        return $vars;
     }
 
 }
